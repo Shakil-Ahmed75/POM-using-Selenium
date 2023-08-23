e00434e8608bbf7fbcc3be29e4f33983b89e292a
Shakil <shuvroahmed959@gmail.com>
2023-08-23 20:41:42 +0600

    Added to github

---
+++ PomUsingSelenium/src/main/java/PomUsingSelenium/PomUsingSelenium/App.java
@@ -0,0 +1,13 @@
+package PomUsingSelenium.PomUsingSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
