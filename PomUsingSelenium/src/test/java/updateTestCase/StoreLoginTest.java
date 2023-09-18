d3bb1b24c0b824440a71165133463b3bcc464d89
Shakil <shuvroahmed959@gmail.com>
2023-09-18 20:34:24 +0600

    update

---
+++ PomUsingSelenium/src/test/java/updateTestCase/StoreLoginTest.java
@@ -0,0 +1,25 @@
+package updateTestCase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import updatePageObjectModel.TestStoreLoginPage;
+
+public class StoreLoginTest {
+
+	@Test
+	public void login() {
+		WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://teststore.automationtesting.co.uk/login?back=my-account");
+     
+        TestStoreLoginPage logPage = new TestStoreLoginPage(driver);
+        logPage.getEmail().sendKeys("test@test.com");
+        logPage.getPassword().sendKeys("test123");
+        logPage.getloginBtn().click();
+        driver.quit();
+        
+		
+	}
+}
